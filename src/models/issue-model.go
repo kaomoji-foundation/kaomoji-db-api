7c87ccfa3fa8ce18d7b1deed993aa9fde7361408
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-05-12 04:11:20 +0200

    🚧 ✨ kaomoji & issue models, radix slice sort

---
+++ src/models/issue-model.go
@@ -0,0 +1,153 @@
+package models
+
+import (
+	"context"
+	"log"
+	"reflect"
+	"time"
+
+	"GO-API-template/src/services"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type Issue struct {
+	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Author      UserMinimal        `bson:"author,omitempty" json:"author"`
+	Reviewer    UserMinimal        `bson:"reviewer,omitempty" json:"reviewer"`
+	StartDate   time.Time          `bson:"startDate,omitempty" json:"startDate"`
+	EndDate     time.Time          `bson:"endDate,omitempty" json:"endDate"`
+	Title       string             `bson:"title" json:"title"`
+	Description string             `bson:"description,omitempty" json:"description"`
+	// Status: "acepted", "closed", "on review", "assigned", "pending"
+	Status string `bson:"status" json:"status"`
+	// Operation: "create", "modify", "delete", "report"
+	Operation string  `bson:"operation" json:"operation"`
+	Kaomoji   Kaomoji `bson:"kaomoji" json:"kaomoji"`
+	Solution  Kaomoji `bson:"solution" json:"solution"`
+}
+
+// basic reduced issuedata viewable by anyone to use on issues listings
+type IssueMinimal struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Author    UserMinimal        `bson:"author,omitempty" json:"author"`
+	Reviewer  UserMinimal        `bson:"reviewer,omitempty" json:"reviewer"`
+	Title     string             `bson:"title" json:"title"`
+	Status    string             `bson:"status" json:"status"`
+	Operation string             `bson:"operation" json:"operation"`
+	Kaomoji   KaomojiMinimal     `bson:"kaomoji" json:"kaomoji"`
+}
+
+//? The plurificated interfaces of the models are probably useless AAAND anoying
+type Issues interface {
+	Create() *mongo.InsertOneResult
+	ReadAll() []Issue
+	CreateSingletonDBAndCollection()
+}
+
+var IssuesCollection *mongo.Collection
+
+// this is the database where the collection is expected, could have multiple if necessary
+var issueModelDB *mongo.Database
+
+// Returns the public/viewable issue info
+func (i Issue) Minimal() IssueMinimal {
+	return IssueMinimal{
+		ID:        i.ID,
+		Author:    i.Author,
+		Reviewer:  i.Reviewer,
+		Status:    i.Status,
+		Operation: i.Operation,
+		Kaomoji:   i.Kaomoji.Minimal(),
+	}
+}
+
+func (i Issue) CreateSingletonDBAndCollection() {
+	if issueModelDB == nil {
+		issueModelDB = services.Mongo.DBs["mainDB"]
+	}
+	if IssuesCollection == nil {
+		IssuesCollection = issueModelDB.Collection("issues")
+	}
+}
+
+func (i *Issue) Create() (*mongo.InsertOneResult, error) {
+	i.CreateSingletonDBAndCollection()
+
+	insertedRes, err := IssuesCollection.InsertOne(context.Background(), i)
+	if err != nil {
+		log.Println(err)
+	}
+	i.ID = insertedRes.InsertedID.(primitive.ObjectID)
+
+	return insertedRes, err
+}
+
+func (i *Issue) Update() error {
+	_, err := IssuesCollection.UpdateByID(context.Background(), i.ID, i)
+	return err
+}
+
+func (i Issue) ReadAll() []Issue {
+	i.CreateSingletonDBAndCollection()
+
+	filter := bson.D{
+		//{"name", "uwu"}, // this works, Try it!
+	}
+
+	currsor, err := IssuesCollection.Find(context.Background(), filter)
+	if err != nil {
+		panic(err)
+	}
+	defer currsor.Close(services.Mongo.Context)
+
+	var issues []Issue
+	currsor.All(context.Background(), &issues)
+
+	return issues
+}
+
+// fills the issue checking id
+func (i *Issue) Fill(idHex string) error {
+
+	id, err := primitive.ObjectIDFromHex(idHex)
+	if err != nil {
+		return err
+	}
+
+	filter := bson.M{"_id": id}
+	res := IssuesCollection.FindOne(context.Background(), filter)
+	if err := res.Err(); err != nil {
+		return err
+	}
+	err = res.Decode(i)
+	return err
+}
+
+// Sets the status to "acepted" and adds and apply the revision to the propper kaomoji
+func (i *Issue) Accept() error {
+	// i.Kaomoji should only contain ID and the fields to update
+
+	var current Kaomoji
+	err := current.Fill(i.Kaomoji.ID.Hex(), true, false)
+	if err != nil {
+		return err
+	}
+
+	i.Status = "acepted"
+	err = i.Update()
+	if err != nil {
+		return err
+	}
+
+	// Do nothing to the Kaomoji if if is already up to date OR if is empty
+	if reflect.DeepEqual(current, i.Solution) || reflect.DeepEqual(i.Solution, Kaomoji{}) {
+		return nil
+	}
+
+	err = i.Solution.Update()
+
+	return err
+}
---
+++ src/models/kaomoji-model.go
@@ -0,0 +1,278 @@
+package models
+
+import (
+	"context"
+	"errors"
+	"log"
+
+	"GO-API-template/src/services"
+	"GO-API-template/src/utils/radix"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Kaomoji struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	String     []rune             `bson:"string,omitempty" json:"string"`
+	Desciption string             `bson:"description,omitempty" json:"description"`
+	Categories []string           `bson:"categories,omitempty" json:"categories"`
+	Revisions  []IssueMinimal     `bson:"revisions,omitempty" json:"revisions"`
+}
+
+//? The plurificated interfaces of the models are probably useless AAAND anoying
+type Kaomojis interface {
+	Create() *mongo.InsertOneResult
+	ReadAll() []Kaomoji
+	CreateSingletonDBAndCollection()
+}
+
+// basic reduced kaomojidata viewable by anyone to use on kaomojis listings
+type KaomojiMinimal struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	String     []rune             `bson:"string,omitempty" json:"string"`
+	Desciption string             `bson:"description,omitempty" json:"description"`
+	Categories []string           `bson:"categories,omitempty" json:"categories"`
+}
+
+var KaomojisCollection *mongo.Collection
+
+// this is the database where the collection is expected, could have multiple if necessary
+var kaomojiModelDB *mongo.Database
+
+const ErrPolicyCategoriesLength = "Error: ilegal Categories slice size"
+
+// Returns the public/viewable kaomoji info
+func (k Kaomoji) Minimal() KaomojiMinimal {
+	return KaomojiMinimal{
+		ID: k.ID,
+	}
+}
+
+func (k Kaomoji) CreateSingletonDBAndCollection() {
+	if kaomojiModelDB == nil {
+		kaomojiModelDB = services.Mongo.DBs["mainDB"]
+	}
+	if KaomojisCollection == nil {
+		KaomojisCollection = kaomojiModelDB.Collection("kaomojis")
+	}
+}
+
+func (k *Kaomoji) Create() (*mongo.InsertOneResult, error) {
+	k.CreateSingletonDBAndCollection()
+
+	if len(k.Categories) > 10 {
+		return nil, errors.New(ErrPolicyCategoriesLength)
+	}
+	radix.Sort(k.Categories)
+
+	insertedRes, err := KaomojisCollection.InsertOne(context.Background(), k)
+	if err != nil {
+		log.Println(err)
+	}
+	k.ID = insertedRes.InsertedID.(primitive.ObjectID)
+
+	return insertedRes, err
+}
+
+func (k Kaomoji) ReadAll() []Kaomoji {
+	k.CreateSingletonDBAndCollection()
+
+	filter := bson.D{
+		//{"name", "uwu"}, // this works, Try it!
+	}
+
+	currsor, err := KaomojisCollection.Find(context.Background(), filter)
+	if err != nil {
+		panic(err)
+	}
+	defer currsor.Close(services.Mongo.Context)
+
+	var kaomojis []Kaomoji
+	currsor.All(context.Background(), &kaomojis)
+
+	return kaomojis
+}
+
+// fills the kaomoji checking  id, string or email, multiple of them can be used at the same time
+func (k *Kaomoji) Fill(identificator string, id, _string bool) error {
+	var fields = []bson.M{}
+	if id {
+		id, err := primitive.ObjectIDFromHex(identificator)
+		if err == nil {
+			fields = append(fields, bson.M{"_id": id})
+		}
+	}
+
+	if _string {
+		fields = append(fields, bson.M{"string": identificator})
+	}
+
+	filter := bson.M{"$or": fields}
+
+	res := KaomojisCollection.FindOne(context.Background(), filter)
+	if err := res.Err(); err != nil {
+		return err
+	}
+	res.Decode(k)
+	return nil
+}
+
+// Fills the kaomoji checking  id, string or email, multiple of them can be used at the same time
+func (k *Kaomoji) FillMinimal(identificator string, id, _string bool) (KaomojiMinimal, error) {
+	var fields = []bson.M{}
+	if id {
+		id, err := primitive.ObjectIDFromHex(identificator)
+		if err == nil {
+			fields = append(fields, bson.M{"_id": id})
+		}
+	}
+	if _string {
+		fields = append(fields, bson.M{"string": identificator})
+	}
+
+	var min KaomojiMinimal
+
+	filter := bson.M{"$or": fields}
+	projection := bson.M{
+		"_id":         1,
+		"string":      1,
+		"description": 1,
+		"categories":  1,
+	}
+	opts := &options.FindOneOptions{Projection: projection}
+	res := KaomojisCollection.FindOne(context.Background(), filter, opts)
+	if err := res.Err(); err != nil {
+		return min, err
+	}
+	res.Decode(k)
+
+	return k.Minimal(), nil
+}
+
+func (k *Kaomoji) Update() error {
+	if len(k.Categories) > 10 {
+		return errors.New(ErrPolicyCategoriesLength)
+	}
+	radix.Sort(k.Categories)
+	_, err := IssuesCollection.UpdateByID(context.Background(), k.ID, k)
+	return err
+}
+
+// Will only return false if it finds a diferent kaomoji with the same string
+func (k Kaomoji) CheckUnique() (bool, error) {
+
+	filter := bson.M{
+		"_id":    bson.M{"$ne": k.ID},
+		"string": k.String,
+	}
+
+	count, err := UsersCollection.CountDocuments(context.Background(), filter)
+
+	return count == 0, err
+}
+
+//TODO: GetSimilar() ([]KaomojiMinimal, error) {...} get kaomojis with similar keys, or strings (using a threshold)
+/*
+Get kaomojis with similar keys, or strings, Only one of the requirements have to match tobe considered similar
+ 	- keys, the minimum amount (inclusive) of keys to match, 0 will completely skip this checking
+ 	- _string, the maximum fuzyness in characters in trimed search, 0 will skip checking
+*/
+func (k Kaomoji) GetSimilar(keysThreshold int, stringFuzyness int) ([]KaomojiMinimal, error) {
+	filter := bson.M{}
+	projection := bson.M{
+		"_id":         1,
+		"string":      1,
+		"description": 1,
+		"categories":  1,
+	}
+	opts := &options.FindOptions{Projection: projection}
+	cursor, err := KaomojisCollection.Find(context.Background(), filter, opts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var res []KaomojiMinimal
+
+	for cursor.Next(context.Background()) {
+		var kao KaomojiMinimal
+		err = cursor.Decode(&kao)
+		if err != nil {
+			return nil, err
+		}
+
+		similarStr := stringFuzyness != 0 && fuzzyStringIsIn(kao.String, k.String, stringFuzyness)
+		if similarStr || matchedStringSlice(k.Categories, kao.Categories) >= keysThreshold {
+			res = append(res, kao)
+		}
+	}
+
+	return res, nil
+}
+
+/*
+Returns the amount of matched items in the two string slices
+*/
+func matchedStringSlice(s1, s2 []string) int {
+	count := 0
+	set := make(map[string]bool)
+	for _, v := range s1 {
+		set[v] = true
+	}
+	for _, v := range s2 {
+		if set[v] {
+			count++
+		}
+	}
+	return count
+}
+
+//TODO: fuzzyStringIsIn(stri, subject []rune, fuzyness ...int) bool {...}
+/*
+Trims str checking against subject in a fuzy way, and checks if subject is within str
+str string to find subject in
+subjet string to be found within str in a fuzy way
+Fuzyness defaults to 2, so at most 2 operations[insertions, deletions or substitutions] on the trimed string
+*/
+func fuzzyStringIsIn(stri, subject []rune, fuzyness ...int) bool {
+
+	return false
+}
+
+func levenshtein(str1, str2 []rune) int {
+	s1len := len(str1)
+	s2len := len(str2)
+	column := make([]int, len(str1)+1)
+
+	for y := 1; y <= s1len; y++ {
+		column[y] = y
+	}
+	for x := 1; x <= s2len; x++ {
+		column[0] = x
+		lastkey := x - 1
+
+		for y := 1; y <= s1len; y++ {
+			oldkey := column[y]
+			var inc int
+			if str1[y-1] != str2[x-1] {
+				inc = 1
+			}
+			column[y] = minimum(column[y]+1, column[y-1]+1, lastkey+inc)
+			lastkey = oldkey
+		}
+	}
+	return column[s1len]
+}
+func minimum(a, b, c int) int {
+	if a < b {
+		if a < c {
+			return a
+		}
+	} else if b < c {
+		return b
+	}
+	return c
+}
